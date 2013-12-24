d8c72d7e49d30e601a0ff7cfaf1396a86e82e152
Denis Komlev <heavytalks@gmail.com>
2013-12-24 11:44:26 +0300

    Cookie management methods added

--- KYiiHelper.php
+++ KYiiHelper.php
@@ -4,7 +4,7 @@
  * Helper class to provide easier access to functionality which is
  * already included in Yii but sometimes a is bit difficult to remember.
  *
- * @version 1.0.0 (2013-09-19)
+ * @version 1.0.1 (2013-12-24)
  * @author Denis Komlev <deniskomlev@hotmail.com>
  */
 class KYiiHelper
@@ -18,5 +18,31 @@ class KYiiHelper
     {
         $validator = new CEmailValidator;
         return $validator->validateValue($email);
+    }
+
+    // ------------------------------------------------------------------------
+
+    static public function getCookie($name)
+    {
+        return Yii::app()->request->cookies->contains($name)
+            ? Yii::app()->request->cookies[$name]->value
+            : null;
+    }
+
+    // ------------------------------------------------------------------------
+
+    static public function setCookie($name, $value, $options = array())
+    {
+        if (isset($options['time'])) {
+            $options['expire'] = time() + $options['time'];
+        }
+        Yii::app()->request->cookies[$name] = new CHttpCookie($name, $value, $options);
+    }
+
+    // ------------------------------------------------------------------------
+
+    static public function removeCookie($name, $options = array())
+    {
+        Yii::app()->request->cookies->remove($name, $options);
     }
 }
